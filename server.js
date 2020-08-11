adf13a6d7a3017bccc69a7d68deba4fc784c8c71
Rishikesh <contactrishikeshhere@gmail.com>
2020-08-11 23:25:41 +0530

    connected to db

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const knex = require('knex');
+
+const db=knex({
+    client: 'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'postgres',
+        password:'root',
+        database:'i-love-c'
+    }
+})
+
+
+// db.select('*').from('users').then(data => {
+//     console.log(data);
+// });
 
 const app = express();
 
 const database = {
     users:[
         {
-            teamName:"india",
-            whatsappNumber:9003,
+            tName:"india",
+            wNumber:9003,
         },
         {
-            teamName:"india",
-            whatsappNumber:9092,
+            tName:"india",
+            wNumber:9092,
         },
     ]
 }
@@ -24,20 +40,32 @@ app.use(cors());
 
 
 app.get('/', (req,res)=> {
-    res.send('this hello buisness')
+    res.send(database.users)
 })
 
 app.post('/submission', (req,res)=> {
     const { tName, wNumber } = req.body;
-    database.users.push({
-        teamName:tName,
-        whatsappNumber: wNumber,
-        joined: new Date(),
-    })
-    res.json(database.users[database.users.length-1]);
+
+    db('users')
+        .returning('*')
+        .insert({
+            tname : tName,
+            wnumber : wNumber,
+            joined : new Date(),
+        })
+        .then(user=> {
+            res.json(user);
+        })
+        .catch(err=> res.status(400).json('unable to register'))
+    // database.users.push({
+    //     teamName:tName,
+    //     whatsappNumber: wNumber,
+    //     joined: new Date().toString(),
+    // })s
+    // res.json(database.users[database.users.length-1]);
 })
 
-console.log(database.users);
+// console.log(database.users);
 
 app.listen(3000, () => {
     console.log("App running on port 3000");
